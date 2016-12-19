94322dd948199fcf7b8b6298af05197219592dfa
José Alberto Martín <jalbertomartinfalcon@gmail.com>
2016-12-19 17:59:49 +0100

    Initial Commit

---
+++ TempConverter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TempConverter
+//
+//  Created by José Alberto Martín Falcón on 19/12/16.
+//  Copyright © 2016 José Alberto Martín Falcón. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
